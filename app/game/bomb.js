6b60b2fc8d590d24df67023d47c305493e7ea5f1
cojmar <cojmar@gmail.com>
2024-04-17 16:57:08 +0300

    bomb tiles

--- app/game/bomb.js
+++ app/game/bomb.js
@@ -26,6 +26,20 @@ export class Bomb extends GameObject {
         })
         if (this.time() > this.data.time * 1000) {
             if (this?.data?.player === this.scene.sys.game.net.me.info.user) this.scene.bombs++
+            let bomb_tile = this.get_tile()
+
+            let tiles_to_brake = []
+
+            for (let x = bomb_tile.x - this.data.range; x <= bomb_tile.x + this.data.range; x++) tiles_to_brake.push(this.scene.map.map.getTileAt(x, bomb_tile.y))
+            for (let y = bomb_tile.y - this.data.range; y <= bomb_tile.y + this.data.range; y++) tiles_to_brake.push(this.scene.map.map.getTileAt(bomb_tile.x, y))
+
+            tiles_to_brake.map(t => {
+                if (this.scene.map.brake_tile(t) && this.data.player === this.scene.sys.game.net.me.info.user) {
+                    this.scene.bombs++
+                }
+            })
+
+
             this.destroy()
 
         }
