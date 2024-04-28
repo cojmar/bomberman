45948d2826238be0692488ee234a5e7bcc218162
cojmar <cojmar@gmail.com>
2024-04-28 13:51:29 +0300

    proper explosion

--- app/game/bomb.js
+++ app/game/bomb.js
@@ -36,42 +36,84 @@ export class Bomb extends GameObject {
         let update = { bombs: player?.data.bombs || 0, range: player?.data.range || 0 }
         update.bombs++
 
-
-
         let bomb_tile = this.get_tile()
-
         let tiles_to_brake = []
 
-        for (let x = bomb_tile.x - this.data.range; x <= bomb_tile.x + this.data.range; x++) tiles_to_brake.push(this.scene.map.map.getTileAt(x, bomb_tile.y))
-        for (let y = bomb_tile.y - this.data.range; y <= bomb_tile.y + this.data.range; y++) tiles_to_brake.push(this.scene.map.map.getTileAt(bomb_tile.x, y))
+        for (let x = bomb_tile.x; x <= bomb_tile.x + this.data.range; x++) {
+            let t = this.scene.map.map.getTileAt(x, bomb_tile.y)
+            if (!t) break
+            if (this.scene.map.brakeable_tiles.includes(t.oindex) && t.index !== 1) {
+                tiles_to_brake.push(t)
+                break
+            } else {
+                if (this.scene.map.collisions.includes(t.oindex)) break
+                if (this.scene.map.safe_spots.includes(t.oindex)) break
+                tiles_to_brake.push(t)
+            }
+        }
+
+        for (let x = bomb_tile.x - 1; x >= bomb_tile.x - this.data.range; x--) {
+            let t = this.scene.map.map.getTileAt(x, bomb_tile.y)
+            if (!t) break
+            if (this.scene.map.brakeable_tiles.includes(t.oindex) && t.index !== 1) {
+                tiles_to_brake.push(t)
+                break
+            } else {
+                if (this.scene.map.collisions.includes(t.oindex)) break
+                if (this.scene.map.safe_spots.includes(t.oindex)) break
+                tiles_to_brake.push(t)
+            }
+        }
+        for (let y = bomb_tile.y + 1; y <= bomb_tile.y + this.data.range; y++) {
+            let t = this.scene.map.map.getTileAt(bomb_tile.x, y)
+            if (!t) break
+            if (this.scene.map.brakeable_tiles.includes(t.oindex) && t.index !== 1) {
+                tiles_to_brake.push(t)
+                break
+            } else {
+                if (this.scene.map.collisions.includes(t.oindex)) break
+                if (this.scene.map.safe_spots.includes(t.oindex)) break
+                tiles_to_brake.push(t)
+            }
+        }
+        for (let y = bomb_tile.y - 1; y >= bomb_tile.y - this.data.range; y--) {
+            let t = this.scene.map.map.getTileAt(bomb_tile.x, y)
+            if (!t) break
+            if (this.scene.map.brakeable_tiles.includes(t.oindex) && t.index !== 1) {
+                tiles_to_brake.push(t)
+                break
+            } else {
+                if (this.scene.map.collisions.includes(t.oindex)) break
+                if (this.scene.map.safe_spots.includes(t.oindex)) break
+                tiles_to_brake.push(t)
+            }
+        }
+
+
+
+
+
 
         tiles_to_brake.map(t => {
 
             if (this.scene.map.brake_tile(t)) {
                 update.bombs++
                 update.range++
-
             }
-            if (t) {
-                let flame = this.scene.add.particles(t.pixelX + (t.baseWidth / 2), t.pixelY + (t.baseHeight / 2), 'flares',
-                    {
-                        frame: 'white',
-                        color: [0xfacc22, 0xf89800, 0xf83600, 0x9f0404],
-                        colorEase: 'quad.out',
-                        lifespan: 500,
-                        scale: { start: 0.70, end: 0, ease: 'sine.out' },
-                        speed: 10,
-                        advance: 500,
-                        frequency: 50,
-                        blendMode: 'ADD',
-                        duration: 100,
-                    })
-                this.scene.game_layer.add(flame)
-            }
-
-
-
-
+            let flame = this.scene.add.particles(t.pixelX + (t.baseWidth / 2), t.pixelY + (t.baseHeight / 2), 'flares',
+                {
+                    frame: 'white',
+                    color: [0xfacc22, 0xf89800, 0xf83600, 0x9f0404],
+                    colorEase: 'quad.out',
+                    lifespan: 500,
+                    scale: { start: 0.70, end: 0, ease: 'sine.out' },
+                    speed: 10,
+                    advance: 500,
+                    frequency: 50,
+                    blendMode: 'ADD',
+                    duration: 100,
+                })
+            this.scene.game_layer.add(flame)
         })
         if (player) player.set_data(update)
         this.delete()
